d9b57f7556ec10dac497c3e63949f3e5a4ea7b9b
Felix Gnass <fgnass@gmail.com>
2022-06-11 02:39:44 +0200

    fix: use history instead of hash

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
