011bab634f6efae7c0b936c4a50625388cdfeb5f
Felix Gnass <fgnass@gmail.com>
2022-06-13 00:40:03 +0200

    fix: use relative base path for gh_pages

--- examples/preact/vite.config.ts
+++ examples/preact/vite.config.ts
@@ -3,6 +3,7 @@ import preact from "@preact/preset-vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "",
   plugins: [
     preact(),
     capri({
--- examples/solid/vite.config.ts
+++ examples/solid/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "",
   plugins: [
     solid({
       ssr: true,
