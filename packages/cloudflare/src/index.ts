1c7b91146473f4445721babe3eb179d0330bd002
Felix Gnass <fgnass@gmail.com>
2022-07-10 11:56:19 +0200

    feat: create 404.html if it does not exist

--- packages/cloudflare/src/index.ts
+++ packages/cloudflare/src/index.ts
@@ -16,19 +16,29 @@ export default function cloudflare(): BuildTarget {
       const dirName = path.dirname(new URL(import.meta.url).pathname);
       const funcDir = path.resolve(outDir, "functions");
 
+      // Cloudflare treats projects without a 404 page as SPA.
+      // To enable MPA mode we add a basic error page if no custom one exists.
+      const notFound = path.resolve(outDir, "404.html");
+      if (!fsutils.exists(notFound)) {
+        fsutils.copy(
+          path.resolve(dirName, "..", "files", "404.html"),
+          notFound
+        );
+      }
+
       // Copy the handler
       fsutils.copy(
         path.resolve(dirName, "middleware.js"),
         path.resolve(funcDir, "_middleware.js"),
         {
           replace: {
-            "virtual:capri-ssr": "./ssr.js",
+            "virtual:capri-ssr": "./_ssr.js",
           },
         }
       );
 
       // Copy the ssrBundle
-      fsutils.copy(ssrBundle, path.resolve(funcDir, "ssr.js"));
+      fsutils.copy(ssrBundle, path.resolve(funcDir, "_ssr.js"));
     },
   };
 }
