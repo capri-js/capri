7dfb6d50d4f249b1d6d0e07f9ff6cafd27ec927b
Felix Gnass <fgnass@gmail.com>
2022-07-10 11:56:27 +0200

    docs: add comments

--- packages/cloudflare/src/middleware.ts
+++ packages/cloudflare/src/middleware.ts
@@ -1,12 +1,15 @@
 import ssr from "virtual:capri-ssr";
 
 const handler: PagesFunction = async ({ request, next }) => {
-  const path = new URL(request.url).pathname;
+  // Handle the request as usual...
   const response = await next();
   if (
     response.status === 404 &&
     request.headers.get("accept")?.includes("text/html")
   ) {
+    // No static asset was found and the browser accepts html.
+    // Try to render the requested page...
+    const path = new URL(request.url).pathname;
     const headers = new Headers({
       "Content-Type": "text/html; charset=utf-8",
     });
@@ -14,7 +17,9 @@ const handler: PagesFunction = async ({ request, next }) => {
       headers: request.headers,
       setHeader: headers.set.bind(headers),
     });
-    return new Response(html, { headers });
+    if (html) {
+      return new Response(html, { headers });
+    }
   }
   return response;
 };
