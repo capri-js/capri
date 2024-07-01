878209f3790316657c5f4205f2b80bacaf535291
Felix Gnass <fgnass@gmail.com>
2024-07-01 20:45:02 +0200

    refactor: use degit

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # ![Capri](logo.svg)
 
+Capri is a site generator for React/Preact.
+
+Capri supports two different rendering modes: one is optimized for speed and minimal page weight,
+whereas the other one is optimized for visual editing and live previews inside your CMS.
+
+For regular visitors, Capri sites work much like Astro pages, where only the interactive parts (islands) get hydrated.
+For the static parts, no JavaScript needs to be shipped to the client, resulting in very fast load times.
+
+For editors on the other hand, Capri pages can also be rendered as single page apps, providing a snappy user experience when combined with a CMS that supports visual editing like TinaCMS, Sanity or Storyblok.
+
 ## TODO
 
 - Simple create-capri implementation based on degit, ora and prompts
--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,6 +1,57 @@
 #!/usr/bin/env node
 
-//TODO
-console.log("TODO");
+import arg from "arg";
+import degit from "degit";
+import ora from "ora";
+import prompts from "prompts";
 
-export {};
+const templates = [
+  { title: "React", value: "capri-js/capri/examples/react" },
+  { title: "Preact", value: "capri-js/capri/examples/preact" },
+];
+
+const args = arg({
+  "--force": Boolean,
+});
+
+const [dir, template] = args._;
+
+const overrides: Record<string, string> = {};
+if (dir) overrides.dir = dir;
+
+if (template) {
+  if (template.includes("/")) {
+    overrides.template = template;
+  } else {
+    const option = templates.find(
+      (t) => t.title.toLowerCase() === template.toLowerCase()
+    );
+    if (option) {
+      overrides.template = option.value;
+    }
+  }
+}
+prompts.override(overrides);
+
+const opts = await prompts([
+  {
+    name: "dir",
+    type: "text",
+    message: "Directory name",
+    initial: "my-capri-site",
+  },
+  {
+    name: "template",
+    type: "select",
+    message: "Template",
+    choices: templates,
+  },
+]);
+
+const spinner = ora(`Downloading ${opts.template}`).start();
+try {
+  await degit(opts.template, { force: args["--force"] }).clone(opts.dir);
+  spinner.succeed(`Created ${opts.dir}`);
+} catch (err: any) {
+  spinner.fail(err.message);
+}
