a687e0cd82233a9fdc7641d87f7d7c0fbed40c3c
Felix Gnass <fgnass@gmail.com>
2025-01-06 21:45:42 +0100

    fix: emit assets in ssr build

--- packages/capri/src/vite-plugin.ts
+++ packages/capri/src/vite-plugin.ts
@@ -96,6 +96,7 @@ export function capri({
             build: {
               ssr,
               emptyOutDir: false, // keep the client build
+              emitAssets: true,
               rollupOptions,
             },
           };
