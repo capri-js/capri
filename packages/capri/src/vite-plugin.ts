0b13b8867efa10d19d343a52c13b63baf5be78d6
Felix Gnass <fgnass@gmail.com>
2025-03-10 18:59:38 +0100

    style: format code

--- packages/capri/src/vite-plugin.ts
+++ packages/capri/src/vite-plugin.ts
@@ -100,7 +100,7 @@ export function capri({
             // index.html points to a .server.* file
             if (spa) {
               throw new Error(
-                "In order to generate an SPA, index.html must point to a client entry file."
+                "In order to generate an SPA, index.html must point to a client entry file.",
               );
             }
           } else if (spa) {
