b06116cc8eab20709810a88e73983b7a22be7b48
Felix Gnass <fgnass@gmail.com>
2026-02-02 10:50:48 +0100

    feat: add classname-variants to templates

--- templates/react/components/ui/button.tsx
+++ templates/react/components/ui/button.tsx
@@ -1,10 +1,20 @@
-import { ComponentProps } from "react";
+import { styled } from "classname-variants/react";
 
-export function Button(props: ComponentProps<"button">) {
-  return (
-    <button
-      {...props}
-      className="inline-flex items-center bg-green-600 hover:bg-green-500 text-white px-2 py-0.5 rounded-sm font-semibold cursor-pointer"
-    />
-  );
-}
+export const Button = styled("button", {
+  base: "inline-flex items-center rounded-sm font-semibold cursor-pointer",
+  variants: {
+    color: {
+      primary: "bg-green-600 hover:bg-green-500 text-white",
+      secondary: "bg-gray-200 hover:bg-gray-300 text-gray-800",
+    },
+    size: {
+      sm: "px-2 py-0.5 text-sm",
+      md: "px-3 py-1",
+      lg: "px-4 py-2 text-lg",
+    },
+  },
+  defaultVariants: {
+    color: "primary",
+    size: "md",
+  },
+});
